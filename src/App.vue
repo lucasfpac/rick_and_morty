2ed9ba3ee879f8e7f7c072161ed46262714b0b6c
lucasfpac <lucasfortunato1@gmail.com>
2023-05-29 16:34:17 +0100

    modal update

--- src/App.vue
+++ src/App.vue
@@ -4,41 +4,66 @@
       <div class="flex items-center justify-between mb-8">
         <h1 class="text-3xl font-bold">Build Your Team</h1>
         <div class="flex">
-          <SearchInput v-model="searchTerm" @input="searchCharacters" />
+          <input type="text" v-model="searchTerm" @input="searchCharacters" placeholder="Search characters" class="border border-gray-300 rounded-l px-4 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500">
           <button @click="clearSearch" class="bg-gray-300 text-gray-600 rounded-r px-4 py-2 hover:bg-gray-400 focus:outline-none focus:ring-2 focus:ring-blue-500">Clear</button>
         </div>
-        <TeamButton @click="openModal" />
+        <button @click="openModal" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded ml-4">Team</button>
       </div>
-      <CharacterGrid v-if="characters.length" :characters="characters" :isSelected="isSelected" @change-colour="changeColour" />
+      <div v-if="characters.length" class="grid grid-cols-1 md:grid-cols-3 gap-4">
+        <div v-for="character in characters" :key="character.id" class="bg-white rounded shadow p-4">
+          <div class="mb-4">
+            <img :src="character.image" :alt="character.name" class="w-full h-auto rounded">
+          </div>
+          <div class="text-center">
+            <h2 class="text-lg font-bold mb-2">{{ character.name }}</h2>
+            <p class="text-gray-600">{{ character.species }}</p>
+            <p class="text-gray-600">{{ character.status }}</p>
+          </div>
+          <div class="mt-4">
+            <button @click="toggleSelection(character)" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+              {{ isSelected(character) ? 'Selected' : 'Select' }}
+            </button>
+          </div>
+        </div>
+      </div>
       <div v-else>
         <p class="text-center">Loading...</p>
       </div>
-      <Pagination :currentPage="currentPage" :totalPages="totalPages" @previous-page="previousPage" @next-page="nextPage" />
-      <TeamModal v-if="isModalOpen" :selectedCharacters="getSelectedCharacters" @close-modal="closeModal" />
+      <div class="mt-6">
+        <button @click="previousPage" :disabled="currentPage === 1" class="bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 rounded-l">
+          Previous
+        </button>
+        <button @click="nextPage" :disabled="currentPage === totalPages" class="bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 rounded-r">
+          Next
+        </button>
+      </div>
+
+      <!-- Team Modal -->
+      <div v-if="isModalOpen" class="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50">
+        <div class="bg-white rounded shadow p-4">
+          <h2 class="text-lg font-bold mb-4">Team Members</h2>
+          <ul>
+            <li v-for="character in selectedCharacters" :key="character.id" class="mb-2">
+              <img :src="character.image" :alt="character.name" class="w-12 h-auto rounded mr-2">
+              <span class="text-gray-800">{{ character.name }}</span>
+              <button @click="removeCharacter(character.id)" class="bg-red-500 hover:bg-red-700 text-white font-bold py-1 px-2 rounded-sm">Remove</button>
+            </li>
+          </ul>
+          <button @click="closeModal" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mt-4">Close</button>
+        </div>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
 import axios from 'axios';
-import CharacterGrid from './components/CharacterGrid.vue';
-import Pagination from './components/Pagination.vue';
-import SearchInput from './components/SearchInput.vue';
-import TeamButton from './components/TeamButton.vue';
-import TeamModal from './components/TeamModal.vue';
 
 export default {
-  components: {
-    CharacterGrid,
-    Pagination,
-    SearchInput,
-    TeamButton,
-    TeamModal
-  },
   data() {
     return {
       characters: [],
-      isSelected: {},
+      selectedCharacters: [],
       currentPage: 1,
       totalPages: 0,
       searchTerm: '',
@@ -50,21 +75,29 @@ export default {
   },
   methods: {
     fetchCharacters() {
-      axios.get(`https://rickandmortyapi.com/api/character/?page=${this.currentPage}`)
+      axios
+        .get(`https://rickandmortyapi.com/api/character/?page=${this.currentPage}`)
         .then(response => {
           this.characters = response.data.results;
-          this.isSelected = this.characters.reduce((acc, character) => {
-            acc[character.id] = false;
-            return acc;
-          }, {});
           this.totalPages = response.data.info.pages;
         })
         .catch(error => {
           console.error(error);
         });
     },
-    changeColour(characterId) {
-      this.isSelected[characterId] = !this.isSelected[characterId];
+    toggleSelection(character) {
+      if (this.isSelected(character)) {
+        this.selectedCharacters = this.selectedCharacters.filter(c => c.id !== character.id);
+      } else {
+        if (this.selectedCharacters.length < 5) {
+          this.selectedCharacters.push(character);
+        } else {
+          alert('Maximum limit of 5 characters reached!');
+        }
+      }
+    },
+    isSelected(character) {
+      return this.selectedCharacters.some(c => c.id === character.id);
     },
     previousPage() {
       if (this.currentPage > 1) {
@@ -80,13 +113,10 @@ export default {
     },
     searchCharacters() {
       if (this.searchTerm) {
-        axios.get(`https://rickandmortyapi.com/api/character/?name=${this.searchTerm}`)
+        axios
+          .get(`https://rickandmortyapi.com/api/character/?name=${this.searchTerm}`)
           .then(response => {
             this.characters = response.data.results;
-            this.isSelected = this.characters.reduce((acc, character) => {
-              acc[character.id] = false;
-              return acc;
-            }, {});
             this.totalPages = response.data.info.pages;
           })
           .catch(error => {
@@ -105,19 +135,9 @@ export default {
     },
     closeModal() {
       this.isModalOpen = false;
-    }
-  },
-  computed: {
-    getSelectedCharacters() {
-      return this.characters.filter(character => this.isSelected[character.id]);
-    }
-  },
-  watch: {
-    isSelected: {
-      handler() {
-        this.$forceUpdate(); // Force re-rendering to update the modal content
-      },
-      deep: true
+    },
+    removeCharacter(characterId) {
+      this.selectedCharacters = this.selectedCharacters.filter(c => c.id !== characterId);
     }
   }
 };
--- src/components/TeamModal.vue
+++ src/components/TeamModal.vue
@@ -1,14 +1,23 @@
 <template>
-    <div class="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50">
-      <div class="bg-white rounded shadow p-4">
+    <div v-if="selectedCharacters.length > 0" class="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50">
+      <div class="bg-white rounded shadow p-4 max-w-lg">
         <h2 class="text-lg font-bold mb-4">Team Members</h2>
         <ul>
-          <li v-for="character in selectedCharacters" :key="character.id" class="mb-2">
-            <img :src="character.image" :alt="character.name" class="w-12 h-auto rounded mr-2">
-            <span class="text-gray-800">{{ character.name }}</span>
+          <li v-for="character in selectedCharacters" :key="character.id" class="mb-6">
+            <div class="flex items-center">
+              <img :src="character.image" :alt="character.name" class="w-32 h-auto rounded mr-4">
+              <div>
+                <h3 class="text-lg font-bold">{{ character.name }}</h3>
+                <p class="text-gray-600">{{ character.species }}</p>
+                <p class="text-gray-600">{{ character.status }}</p>
+                <p class="text-gray-600">{{ character.description }}</p>
+              </div>
+            </div>
+            <button @click="removeCharacter(character.id)" class="bg-red-500 hover:bg-red-700 text-white font-bold py-1 px-2 rounded-sm text-xs">Remove</button>
           </li>
         </ul>
-        <button @click="$emit('close-modal')" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mt-4">Close</button>
+        <button @click="clearTeam" class="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded mt-4">Clear Team</button>
+        <button @click="closeModal" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mt-2">Close</button>
       </div>
     </div>
   </template>
@@ -19,6 +28,17 @@
       selectedCharacters: {
         type: Array,
         required: true
+      }
+    },
+    methods: {
+      removeCharacter(characterId) {
+        this.$emit('remove', characterId);
+      },
+      closeModal() {
+        this.$emit('close');
+      },
+      clearTeam() {
+        this.$emit('clear');
       }
     }
   };
